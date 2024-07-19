c1e92044bdb9ff31f906b52dfb2d3259f4802731
sekkarin <644259021@webmail.npru.ac.th>
2024-07-19 13:16:38 +0700

    feat(sup.schema): create schema

---
+++ src/suppliers/schemas/supplier.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type SupplierDocument = HydratedDocument<Supplier>;
+
+@Schema({
+  timestamps: true,
+})
+export class Supplier {
+  @Prop({
+    required: true,
+    type: String,
+    max: 50,
+    min: 3,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    max: 256,
+    min: 1,
+  })
+  description: string;
+
+  imageBanners: [
+    {
+      type: string;
+    },
+  ];
+
+  profileImage: {
+    type: string;
+    required: true;
+  };
+}
+
+export const SupplierSchema = SchemaFactory.createForClass(Supplier);
