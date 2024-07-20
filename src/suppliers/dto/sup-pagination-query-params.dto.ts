83f3cd7c17b84b9bc4e37b8d59ad6d5033bea6eb
sekkarin <644259021@webmail.npru.ac.th>
2024-07-20 13:10:03 +0700

    fix(sup-pagination): update class validate

--- src/suppliers/dto/sup-pagination-query-params.dto.ts
+++ src/suppliers/dto/sup-pagination-query-params.dto.ts
@@ -4,13 +4,13 @@ import { Type } from "class-transformer";
 export class SupplierPaginationQueryparamsDto {
   @Type(() => Number)
   @IsInt()
-  // @Min(1)
+  @Min(1)
   readonly page?: number = 1;
 
   @Type(() => Number)
   @IsInt()
-  // @Min(1)
-  // @Max(100)
+  @Min(1)
+  @Max(100)
   readonly limit?: number = 10;
 
   @Type(() => String)
